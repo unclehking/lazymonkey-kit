62db41fcaf758357fac4b011c943e4da9b1552a3
unclehking <513935463@qq.com>
2026-08-31 16:30:55 +0800

    拆分音乐功能为独立项目

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 import toast from './plugins/toast'
 
-const isMusicSite = window.location.hostname.toLowerCase() === 'mp3.h5cssjs.com'
-    || import.meta.env.VITE_APP_TARGET === 'music'
-
-const bootstrap = async () => {
-    const rootComponent = isMusicSite
-        ? (await import('./MusicApp.vue')).default
-        : (await import('./App.vue')).default
-    const app = createApp(rootComponent)
-
-    app.use(createPinia())
-    if (!isMusicSite) {
-        const router = (await import('./router')).default
-        app.use(router)
-    }
-    app.use(toast)
-    app.mount('#app')
-}
-
-bootstrap()
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .use(toast)
+    .mount('#app')
